60bf26ef0f3d03ed88f96066b5b5d35f46818298
marioBytes <mario.bytes@gmail.com>
2020-06-13 16:01:29 -0600

    Updates

---
+++ precache-manifest.6adc623aa87026d213e3fbea59ebd537.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba316342097526d9a92680b2aaf1715",
+    "url": "/https//ssbu-2020.github.io/smash-generator/index.html"
+  },
+  {
+    "revision": "452a9f43b10d9f35f8fe",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/css/main.118911b8.chunk.css"
+  },
+  {
+    "revision": "3f40d82897e9845cbd6d",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/js/2.29966b30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/js/2.29966b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452a9f43b10d9f35f8fe",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/js/main.b62c707c.chunk.js"
+  },
+  {
+    "revision": "7f0581ddbc5131c64282",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/js/runtime-main.5bc17f50.js"
+  },
+  {
+    "revision": "71a63a342ad835048454d0f2530ab9bd",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/banjo-and-kazooie.71a63a34.jpg"
+  },
+  {
+    "revision": "221fe1a4222a61b0f701bee3978c06c4",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/bayonetta.221fe1a4.jpg"
+  },
+  {
+    "revision": "675c484201d754f5189660f2196786a5",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/bowser-jr.675c4842.jpg"
+  },
+  {
+    "revision": "ddd6ec3189552844dbf62d6e55b99215",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/bowser.ddd6ec31.jpg"
+  },
+  {
+    "revision": "dafcc66ac3e42ab8c8cceab9f9731c23",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/byleth.dafcc66a.jpg"
+  },
+  {
+    "revision": "813cd6b5a1020da8c366ed1309ab0bc8",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/captain-falcon.813cd6b5.jpg"
+  },
+  {
+    "revision": "431d756422e6cf5140ff312a82c14f51",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/chrom.431d7564.jpg"
+  },
+  {
+    "revision": "c91ee98e675c28ecb5dae6eeffab5d63",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/cloud.c91ee98e.jpg"
+  },
+  {
+    "revision": "fd7f78a5a872bc0a5d357a7a588aac9d",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/corrin-male.fd7f78a5.jpg"
+  },
+  {
+    "revision": "db2f2c7dfde57f2ffbea048db92a215e",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/daisy.db2f2c7d.jpg"
+  },
+  {
+    "revision": "78bef74b998e2c8b172de031bb19c94e",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/dark-pit.78bef74b.jpg"
+  },
+  {
+    "revision": "3b54e10371dedf70bdee5ad89954764c",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/dark-samus.3b54e103.jpg"
+  },
+  {
+    "revision": "1ae3802390313cd02cfae876c13f4ec4",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/diddy-kong.1ae38023.jpg"
+  },
+  {
+    "revision": "6b7d9db8d81fc9acd663fd45ac9cfcbe",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/donkey-kong.6b7d9db8.jpg"
+  },
+  {
+    "revision": "8e704f6187db16a64e1a2212b90c904c",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/dr-mario.8e704f61.jpg"
+  },
+  {
+    "revision": "16c984e9163c6626c49b5ea24eafc155",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/duck-hunt.16c984e9.jpg"
+  },
+  {
+    "revision": "425939c02687eaa1104b60ef47ae9cc6",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/falco.425939c0.jpg"
+  },
+  {
+    "revision": "38048a8da38d139f24fedd746d76dac3",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/fox.38048a8d.jpg"
+  },
+  {
+    "revision": "242d3c007c33c99d813b0a9ae3999fe2",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ganondorf.242d3c00.jpg"
+  },
+  {
+    "revision": "8f00a820576135d075562197f26bbcc7",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/greninja.8f00a820.jpg"
+  },
+  {
+    "revision": "22b99d2cff4fbeac9c2a304f66c2e85d",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/hero.22b99d2c.jpg"
+  },
+  {
+    "revision": "71e2fb2c41819c8f721ff32f552439a0",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ice-climbers.71e2fb2c.jpg"
+  },
+  {
+    "revision": "66ea5001479bb75f8012518ccf0e2117",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ike.66ea5001.jpg"
+  },
+  {
+    "revision": "de4ec9ad3a675bce67624b8a0dc33e18",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/incineroar.de4ec9ad.jpg"
+  },
+  {
+    "revision": "da22fe4c7a4738f93ac8fcefb5faadd7",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/inkling-female.da22fe4c.jpg"
+  },
+  {
+    "revision": "cb0a7ecd223e7f5696bf923ac7809f76",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/isabelle.cb0a7ecd.jpg"
+  },
+  {
+    "revision": "11c51ef07f1288f2d3c23f29be387ffb",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/jigglypuff.11c51ef0.jpg"
+  },
+  {
+    "revision": "6d1405b465745bd22a2bc49a5a5a6827",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/joker.6d1405b4.jpg"
+  },
+  {
+    "revision": "a0bef5455c4ab7ef01bc48ef5cc24925",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ken.a0bef545.jpg"
+  },
+  {
+    "revision": "4c47e2c9885218d967600d8a41c697f1",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/king-dedede.4c47e2c9.jpg"
+  },
+  {
+    "revision": "2c42f6cce9db9d5808f83f4d7b8830fc",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/king-k-rool.2c42f6cc.jpg"
+  },
+  {
+    "revision": "c9a15d16542b52fe7a5521c17d376c97",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/kirby.c9a15d16.jpg"
+  },
+  {
+    "revision": "3bfa28da0869bbc1f7812300da7c51c4",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/link.3bfa28da.jpg"
+  },
+  {
+    "revision": "902dfaf9647ee4b9875e119565e21389",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/little-mac.902dfaf9.jpg"
+  },
+  {
+    "revision": "7218d48a92821ed2c190ce467c265a23",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/lucario.7218d48a.jpg"
+  },
+  {
+    "revision": "46c79e37f2055ae1948181d254d148d5",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/lucas.46c79e37.jpg"
+  },
+  {
+    "revision": "b1d42ccd8b34536ea1c24632306632ce",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/lucina.b1d42ccd.jpg"
+  },
+  {
+    "revision": "e038596931916d2f66c0db63288a6067",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/luigi.e0385969.jpg"
+  },
+  {
+    "revision": "25113305b8fcc4a170c2efa1622c24a9",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mario.25113305.jpg"
+  },
+  {
+    "revision": "e1b8b54c1d58ee0645e834adb98b64a6",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/marth.e1b8b54c.jpg"
+  },
+  {
+    "revision": "d9a542a47894ebd61b41ad2ae40f0381",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mega-man.d9a542a4.jpg"
+  },
+  {
+    "revision": "84193e6731269a7d7de3ac724482e8c7",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/meta-knight.84193e67.jpg"
+  },
+  {
+    "revision": "5eac4d38e14ee190d6c964e7b9f72984",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mewtwo.5eac4d38.jpg"
+  },
+  {
+    "revision": "ba65d6c1438e4235e67cab00b3845c3b",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mii-fighter-gunner.ba65d6c1.jpg"
+  },
+  {
+    "revision": "4e8a58f960c7db85f081462bb0d27912",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mii-fighter-sword.4e8a58f9.jpg"
+  },
+  {
+    "revision": "93703011745595845fc715d9512fca8a",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mii-fighter.93703011.jpg"
+  },
+  {
+    "revision": "eaaf192c63a49696e10d98d09eca49c9",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/mr-game-and-watch.eaaf192c.jpg"
+  },
+  {
+    "revision": "00260b4395955cc0b82d318d329a5f87",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ness.00260b43.jpg"
+  },
+  {
+    "revision": "fb2ad4461d4aa176b25fd52d17c946d6",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/olimar.fb2ad446.jpg"
+  },
+  {
+    "revision": "a56d2e9c5ffd319304136c84547d483a",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/pac-man.a56d2e9c.jpg"
+  },
+  {
+    "revision": "259d8bc3cad56dd45e00cddeabccb722",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/palutena.259d8bc3.jpg"
+  },
+  {
+    "revision": "296b237d0eb2a27e1a6b071edf5abc2c",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/peach.296b237d.jpg"
+  },
+  {
+    "revision": "01c9aa48a8fb7c8d374e0be633413fe8",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/pichu.01c9aa48.jpg"
+  },
+  {
+    "revision": "ef2524fb53bef3c5130ad8501747e10b",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/pikachu.ef2524fb.jpg"
+  },
+  {
+    "revision": "5edadc8655f489b2e43084be7702a647",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/piranha-plant.5edadc86.jpg"
+  },
+  {
+    "revision": "83f132b03e26df546612f3614bea439d",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/pit.83f132b0.jpg"
+  },
+  {
+    "revision": "f8006f22b3ae6ae18ddd40644668c144",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/pokemon-trainer-female.f8006f22.jpg"
+  },
+  {
+    "revision": "15b863061fced9580355429fcf09e1ee",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/richter.15b86306.jpg"
+  },
+  {
+    "revision": "58e69c8aeee051f3f19a051fb4513077",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ridley.58e69c8a.jpg"
+  },
+  {
+    "revision": "0ce33efe926fbe3dda9f95754633b576",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/rob.0ce33efe.jpg"
+  },
+  {
+    "revision": "f8798214dbeef880c973a764f778d732",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/robin-female.f8798214.jpg"
+  },
+  {
+    "revision": "4994445515883c20a4c8ba4fab234470",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/rosalina-and-luma.49944455.jpg"
+  },
+  {
+    "revision": "39ea053866b398f75e5f366ebc4a1338",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/roy-fe.39ea0538.jpg"
+  },
+  {
+    "revision": "76cc7a5253a8032366f1fdc625b6dbe7",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/ryu.76cc7a52.jpg"
+  },
+  {
+    "revision": "b6092e759a7878c70ce3f2ca0928c2c0",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/samus.b6092e75.jpg"
+  },
+  {
+    "revision": "6a25a5dffaaad20d469487ff8e67ceaa",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/sheik.6a25a5df.jpg"
+  },
+  {
+    "revision": "e72715b519ccf6634ac3ec77f79da911",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/shulk.e72715b5.jpg"
+  },
+  {
+    "revision": "eb213e0444c24a9dc3c90d9390009782",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/simon.eb213e04.jpg"
+  },
+  {
+    "revision": "5ebc129493f6e602c61442874adff093",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/snake.5ebc1294.jpg"
+  },
+  {
+    "revision": "1d1e5c42d49a3ca20686ec670e4d1f3c",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/sonic.1d1e5c42.jpg"
+  },
+  {
+    "revision": "002b349118e591dcb34c0a03f2ad32d6",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/terry.002b3491.jpg"
+  },
+  {
+    "revision": "6b1b2ac49d974a45ed5da5ccfd17ef96",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/toon-link.6b1b2ac4.jpg"
+  },
+  {
+    "revision": "bf318821e716bc2277939f130a1dacf1",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/villager.bf318821.jpg"
+  },
+  {
+    "revision": "86ec960ce0d8b6826c79087c23120074",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/wario.86ec960c.jpg"
+  },
+  {
+    "revision": "466704f636bf92c1ed358b44516c8a6a",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/wii-fit-trainer-female.466704f6.jpg"
+  },
+  {
+    "revision": "f026723e79c3f560a7d0955fdaf3e33e",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/wolf.f026723e.jpg"
+  },
+  {
+    "revision": "607ddc10eec888125fdb129c273c58a7",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/yoshi.607ddc10.jpg"
+  },
+  {
+    "revision": "71f0bf5ced860772c7eeb9d63102ed10",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/young-link.71f0bf5c.jpg"
+  },
+  {
+    "revision": "3dc2abb4d6825a8ad991ebac499ecf4e",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/zelda.3dc2abb4.jpg"
+  },
+  {
+    "revision": "01b9bcf4d7eecdf2e58fda5772055a27",
+    "url": "/https//ssbu-2020.github.io/smash-generator/static/media/zero-suit-samus.01b9bcf4.jpg"
+  }
+]);
